18d2ba46c8d31effced2f599eab9431b1adf4b49
matt <matthewjamesfleming@gmail.com>
2017-08-01 16:49:15 -0700

    added server

---
+++ Resume/server.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const parser = require('body-parser')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const session = require('express-session')
+const port = process.env.PORT || 8000;
+const app = express();
+
+const sessionConfig = {
+    secret: 'SessionSecret',
+    resave: false,
+    saveUninitialized: true,
+    name: 'session',
+    rolling: true,
+    cookie: {
+        secure: false,
+        httpOnly: false,
+        maxAge: 3600000
+    }
+};
+
+app.use(parser.urlencoded({ extended: true }));
+app.use(parser.json());
+
+app.use(session(sessionConfig));
+app.use(cookieParser('asdkjldfjksnasudfh'));
+app.use(express.static(path.join(__dirname, 'dist')));
+
+
+
+
+app.use(require('./server/config/routes/catch-all.routes'));
+
+app.listen(port, () => console.log(`listening on port ${ port }`));
